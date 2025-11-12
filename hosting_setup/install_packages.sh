e8f17e89b54e437841b8831b3a333af9a6d73d97
jonahdev <jonahthiele@yahoo.com>
2025-11-12 14:47:11 -0600

    create cron job template to run the program

--- hosting_setup/install_packages.sh
+++ hosting_setup/install_packages.sh
@@ -10,6 +10,7 @@ apt install python3 -y
 apt install zstd -y
 apt install  rpm2cpio -y
 apt install cron -y
+apt install git -y
 
 #create and activate new virtual environment for python
 python3 -m venv ./venv
@@ -22,3 +23,22 @@ pip install -r requirements.txt
 #enable and start cron
 systemctl enable cron
 systemctl start cron
+
+#clone the repo we might have to move this to another repo so I can authenicate and add it to another repo
+
+git clone https://github.com/dsu-cs/projects-glibc-rop-gadgets.git
+
+#setup the origin
+if git remote | grep -q origin; then
+    git remote set-url origin https://github.com/dsu-cs/projects-glibc-rop-gadgets.git
+else
+    git remote add origin https://github.com/dsu-cs/projects-glibc-rop-gadgets.git
+fi
+
+#somewhere along here we need to figure out authenication
+
+#pull the latest main branch
+git pull origin main
+
+#add the script to the cron job
+crontab crontab.txt
